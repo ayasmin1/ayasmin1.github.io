44ec144955ea5711c5d830f48b59fa2f5655ad4e
ayasmin1 <ayasmin1@fordham.edu>
2020-11-08 20:03:25 -0500

    Button

--- buttons.js
+++ buttons.js
@@ -2,7 +2,7 @@ function lightSwitch_on() // calling this variable from the HTML
 {
     document.getElementById('switch').src='ON.png' // get the picture of the on-switch
     document.body.style.backgroundColor = "yellow"; // background color is yellow when button is preessed for light effect
-    document.getElementById("s2").innerHTML= "As Drake woud say <br> Look, I just flipped the switch (flipped, flipped)" // sentence that appeears when you press this button
+    document.getElementById("s2").innerHTML= "As Drake would say <br> Look, I just flipped the switch (flipped, flipped)" // sentence that appeears when you press this button
 }
 
 function lightSwitch_off() // calling this variable from the HTML
